115dda070d21e1ae2758ad70eed27406f1579eb3
Wyatt Rasmussen <wyattrasmussen002@gmail.com>
2026-08-20 15:41:54 -0500

    init

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://example.com',
+  integrations: [
+    react()
+  ],
+  vite: {
+    css: {
+      devSourcemap: true
+    }
+  }
+});
